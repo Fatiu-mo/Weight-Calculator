730b4aca81ef7b9b0f53552fb1eb74a55c13bde6
Fatiu-mo <fathiubabalola34@gmail.com>
2022-10-15 21:22:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,65 +5,25 @@ const planetCol = document.querySelector("#planet-col")
 const resultDisplay = document.querySelector("#result-display")
 
 const planets = [{
-    planet1: {name: "earth", image:"images/earth.png"},
-    planet2: {name: "mars", image:"images/mars.png"},
-    planet3: {name: "jupiter", image:"images/jupiter.png"},
-    planet4: {name: "saturn", image:"images/saturn.png"},
-    planet5: {name: "neptune", image:"images/neptune.png"},
-    planet6: {name: "pluto", image:"images/pluto.png"},
-    planet7: {name: "moon", image:"images/moon.png"},
-    planet8: {name: "mercury", image:"images/mercury.png"},
-    planet9: {name: "venus", image:"images/venus.png"},
-    planet10: {name: "uranus", image: "images/uranus.png"}
+    planet1: {name: "earth", image:"images/earth.png", adg: 9.8},
+    planet2: {name: "mars", image:"images/mars.png", adg: 3.7},
+    planet3: {name: "jupiter", image:"images/jupiter.png", adg: 23.1},
+    planet4: {name: "saturn", image:"images/saturn.png", adg: 9},
+    planet5: {name: "neptune", image:"images/neptune.png", adg: 11},
+    planet6: {name: "pluto", image:"images/pluto.png", adg: 0.7},
+    planet7: {name: "moon", image:"images/moon.png", adg: 1.6},
+    planet8: {name: "mercury", image:"images/mercury.png", adg: 3.7},
+    planet9: {name: "venus", image:"images/venus.png", adg: 8.9},
+    planet10: {name: "uranus", image: "images/uranus.png", adg: 8.7}
 }]
 
-
-class WeightCalc {
-    constructor(mass) {
-        this.mass = mass
-    }
-
-    getEarthWeight() {
-        return this.mass * 9.8
-    }
-
-    getMoonWeight() {
-        return this.mass * 1.6
-    }
-
-    getMercuryWeight() {
-        return this.mass * 3.7
-    }
-
-    getVenusWeight() {
-        return this.mass * 8.9
-    }
-
-    getMarsWeight() {
-        return this.mass * 3.7
-    }
-    
-    getJupiterWeight() {
-        return this.mass * 23.1
-    }
-
-    getSaturnWeight() {
-        return this.mass * 9
-    }
-
-    getUranusWeight() {
-        return this.mass * 8.7
-    }
-
-    getNeptuneWeight() {
-        return this.mass * 11
-    }
-
-    getPlutoWeight() {
-        return this.mass * 0.7
-    }
+const getPlanetWeight = function (mass, adg) {
+	return mass * adg
 }
 
+const getPlanetMass = (mass,adg) => {
+	return (mass * adg) / 9.8
+}
 
 const store = data => {
     localStorage.setItem("data", data)
@@ -75,7 +35,7 @@ select.addEventListener("change", event => {
 })
 
 
-const displayResult = (image, pName, calculatedWeight) => {
+const displayResult = (image, pName, calculatedWeight, calculatedMass) => {
     resultDisplay.innerHTML = `
     <div class="container-sm py-5 my-4" style="background-color: rgba(77, 75, 75, 0.568); max-width: 850px;">
 	<div class="d-flex justify-content-center align-items-center">
@@ -87,6 +47,7 @@ const displayResult = (image, pName, calculatedWeight) => {
 			  	<p class="lead pt-3 text-white mb-1">The weight of the object on <span id="planet-name" class="fw-bold">${pName.toUpperCase()}</span></p>
 					<div id="result" class="d-inline text-light fw-bold fs-2">
 						${calculatedWeight.toFixed(2)}N
+						<p class="fs-4">(${calculatedMass.toFixed(2)}kg)</p>
 					</div>
 				</div>
 			</div>
@@ -98,32 +59,31 @@ const displayResult = (image, pName, calculatedWeight) => {
 
 
 render.addEventListener("click", () => {
-	const weight = new WeightCalc(mass.value)
-    //console.log(weight)
         if (mass.value === "" || select.value === "") {
             alert("input value in all fields")
           } else {
     planets.forEach(planet => {
         if (localStorage.data == "earth") {
-            displayResult(planet.planet1.image, planet.planet1.name, weight.getEarthWeight())
+            displayResult(planet.planet1.image, planet.planet1.name, getPlanetWeight(mass.value, planet.planet1.adg), getPlanetMass(mass.value, planet.planet1.adg))
         } else if (localStorage.data == "mars") {
-            displayResult(planet.planet2.image, planet.planet2.name, weight.getMarsWeight())
+            displayResult(planet.planet2.image, planet.planet2.name, getPlanetWeight(mass.value, planet.planet2.adg), getPlanetMass(
+							mass.value, planet.planet2.adg))
         } else if (localStorage.data == "jupiter") {
-            displayResult(planet.planet3.image, planet.planet3.name, weight.getJupiterWeight())
+            displayResult(planet.planet3.image, planet.planet3.name, getPlanetWeight(mass.value, planet.planet3.adg), getPlanetMass(mass.value, planet.planet3.adg))
         } else if (localStorage.data == "saturn") {
-            displayResult(planet.planet4.image, planet.planet4.name, weight.getSaturnWeight())
+            displayResult(planet.planet4.image, planet.planet4.name, getPlanetWeight(mass.value, planet.planet4.adg), getPlanetMass(mass.value, planet.planet4.adg))
         } else if (localStorage.data == "neptune") {
-            displayResult(planet.planet5.image, planet.planet5.name, weight.getNeptuneWeight())
+            displayResult(planet.planet5.image, planet.planet5.name, getPlanetWeight(mass.value, planet.planet5.adg), getPlanetMass(mass.value, planet.planet5.adg))
         } else if (localStorage.data == "pluto") {
-            displayResult(planet.planet6.image, planet.planet6.name, weight.getPlutoWeight())
+            displayResult(planet.planet6.image, planet.planet6.name, getPlanetWeight(mass.value, planet.planet6.adg), getPlanetMass(mass.value, planet.planet6.adg))
         } else if (localStorage.data == "moon") {
-            displayResult(planet.planet7.image, planet.planet7.name, weight.getMoonWeight())
+            displayResult(planet.planet7.image, planet.planet7.name, getPlanetWeight(mass.value, planet.planet7.adg), getPlanetMass(mass.value, planet.planet7.adg))
         } else if (localStorage.data == "mercury") {
-            displayResult(planet.planet8.image, planet.planet8.name, weight.getMercuryWeight())
+            displayResult(planet.planet8.image, planet.planet8.name, getPlanetWeight(mass.value, planet.planet8.adg), getPlanetMass(mass.value, planet.planet8.adg))
         } else if (localStorage.data == "venus") {
-            displayResult(planet.planet9.image, planet.planet9.name, weight.getVenusWeight())
+            displayResult(planet.planet9.image, planet.planet9.name, getPlanetWeight(mass.value, planet.planet9.adg), getPlanetMass(mass.value, planet.planet9.adg))
         }  else if (localStorage.data == "uranus") {
-          displayResult(planet.planet10.image, planet.planet10.name, weight.getUranusWeight())
+          displayResult(planet.planet10.image, planet.planet10.name, getPlanetWeight(mass.value, planet.planet10.adg), getPlanetMass(mass.value, planet.planet10.adg))
       }
     })  
 	}
